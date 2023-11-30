1a13ff2935e68805912d95369f68047536da1ea2
fabrizio marras <fmarras79@gmail.com>
2023-11-30 23:49:23 +0100

    Finalised Hero - responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,25 @@
+import { styles } from '../styles';
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen mx-auto bg-hero-pattern bg-no-repeat bg-auto bg-right-bottom md:bg-contain'>
+      <div className='absolute inset-0 top-[100px] max-w-7xl mx-auto flex flex-row md:items-center items-start gap-5'>
+        <div className={`${styles.padding} max-md:bg-white/80 h-screen flex flex-col justify-start md:justify-center`}>
+          <h1 className={`${styles.heroHeadText}`}>Production Ready <br />Cloud Environments <br />in Seconds.</h1>
+          <p className={`${styles.heroSubText} max-w-2xl`}>Kubespaces is democratising access to secure, scalable, highly-available cloud compute by transforming complex cloud industry mechanisms into plug-and-play solutions.</p>
+          <div className="flex flex-col xs:flex-row justify-start gap-6 mt-10">
+              <button
+                type="button"
+                className='bg-primary hover:bg-white border border-primary py-2 px-8 outline-none text-white hover:text-primary font-semibold text-[18px] rounded-md xs:w-fit w-full'>Talk to us
+              </button>
+              <button
+                type="button"
+                className='bg-white hover:bg-primary border border-primary py-2 px-8 outline-none text-primary hover:text-white font-semibold text-[18px] rounded-md xs:w-fit w-full'>Try for Free
+              </button>
+            </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/styles.js
+++ src/styles.js
@@ -4,12 +4,12 @@ const styles = {
     padding: "md:px-16 px-6 md:py-16 py-10",
   
     heroHeadText:
-      "font-black text-white lg:text-[80px] md:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
+      "font-medium text-primary lg:text-[46px] md:text-[32px] text-[24px] lg:leading-[60px] mb-8",
     heroSubText:
-      "text-[#dfd9ff] font-medium lg:text-[30px] md:text-[26px] sm:text-[20px] text-[16px] lg:leading-[40px]",
+      "text-[#bbb] font-normal lg:text-[24px] md:text-[18px] text-[16px] lg:leading-[32px]",
   
     sectionHeadText:
-      "text-white font-black md:text-[60px] md:text-[50px] sm:text-[40px] text-[30px]",
+      "font-medium text-primary md:text-[60px] md:text-[50px] sm:text-[40px] text-[30px]",
     sectionSubText:
       "md:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
   };

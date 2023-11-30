3b6753caef080468c029049a546928be52a07750
fabrizio marras <fmarras79@gmail.com>
2023-11-30 22:10:05 +0100

    added herobg image

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,9 @@ module.exports = {
       screens: {
         xs: "450px",
       },
+      backgroundImage: {
+        "hero-pattern": "url('/src/assets/herobg.jpg')",
+      },
     },
   },
   plugins: [],

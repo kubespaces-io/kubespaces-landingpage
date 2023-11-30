d4ab0f0ee2889013a2d4c5679d37dabb7e47982c
fabrizio marras <fmarras79@gmail.com>
2023-11-30 17:46:51 +0100

    Added Assets and constants file

---
+++ src/constants/index.js
@@ -0,0 +1,81 @@
+import {
+    miro,
+    datadog,
+    american,
+    shell,
+    dCloud
+} from "../assets";
+
+const navLinks = [
+    {
+        id: "how-it-works",
+        title: "How it works",
+    },
+    {
+        id: "pricing",
+        title: "Pricing",
+    },
+    {
+        id: "community",
+        title: "Community",
+    },
+    {
+        id: "about",
+        title: "About",
+    }
+];
+
+const navAuth = [
+    {
+        id: "sign-in",
+        title: "Sign In",
+    },
+    {
+        id: "try",
+        title: "Try Kubespaces Free",
+    }
+];
+
+const benefit = [
+    {
+        title: "1. Benefit here",
+        icon: dCloud,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        title: "2. Benefit here",
+        icon: dCloud,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        title: "3. Benefit here",
+        icon: dCloud,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        title: "4. Benefit here",
+        icon: dCloud,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+];
+
+const clients = [
+    {
+        title: "Miro",
+        icon: miro,
+    },
+    {
+        title: "Datadog",
+        icon: datadog,
+    },
+    {
+        title: "American Express",
+        icon: american,
+    },
+    {
+        title: "Shell",
+        icon: shell,
+    },
+];
+
+export { navLinks, navAuth, benefit, clients };

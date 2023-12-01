21863fae8545341061570a931334949c7864eb8e
fabrizio marras <fmarras79@gmail.com>
2023-12-01 17:25:17 +0100

    updated components imports/exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,19 +1,19 @@
 import NavBar from './NavBar';
-import About from './About';
 import Benefits from './Benefits';
 import Clients from './Clients';
 import Hero from './Hero';
 import Pricing from './Pricing';
 import How from './How';
+import About from './About';
 import Footer from './Footer';
 
 export {
     NavBar,
-    About,
     Benefits,
     Clients,
     Hero,
     Pricing,
     How,
-    Footer,
+    About,
+    Footer
 }

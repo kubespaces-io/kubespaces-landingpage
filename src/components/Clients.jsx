910f79fce058172eb5e67730c5bbd05132d6e223
fabrizio marras <fmarras79@gmail.com>
2023-12-01 13:22:40 +0100

    finalised Clients component

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,28 @@
+import { styles } from '../styles';
+import { clients } from '../constants';
+
+import { SectionWrapper } from '../hoc';
+
 const Clients = () => {
   return (
-    <div>Clients</div>
+    <>
+      <div className="">
+        <h2 className={`${styles.heroSubText}`}>Our Clients</h2>
+        <div className={`${styles.paddingY}`}>
+          <ul className="flex justify-around items-center gap-6">
+            {clients.map((client) => (
+              <li key={client.title}>
+                <img 
+                  src={client.icon} 
+                  alt={client.title}
+                  className="w-full h-full max-w-24 max-h-24 object-contain" />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default Clients
+export default SectionWrapper(Clients, 'clients')

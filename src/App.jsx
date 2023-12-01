c4140ef7e8f00625e2ab87b5a7f57e32558db72b
fabrizio marras <fmarras79@gmail.com>
2023-12-01 13:20:29 +0100

    removed divs between components

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,14 @@ import { NavBar, Hero, Clients, Benefits, Pricing, How, About, Footer } from './
 const App = () => {
   return (
     <BrowserRouter>
-    <div>
       <NavBar />
       <Hero />
-    </div>
-    <div>
       <Clients />
-    </div>
-    <div>
       <Benefits />
       <Pricing />
       <How />
       <About />
-    </div>
-    <div>
       <Footer />
-    </div>
-
     </BrowserRouter>
   )
 }

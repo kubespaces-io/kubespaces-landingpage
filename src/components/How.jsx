da700564dca9327e33ccf905c97e635acb0f120c
fabrizio marras <fmarras79@gmail.com>
2023-12-01 17:26:05 +0100

    finalised How it works component

--- src/components/How.jsx
+++ src/components/How.jsx
@@ -1,7 +1,16 @@
+import { styles } from '../styles';
+
+import { SectionWrapper } from '../hoc';
+
 const How = () => {
   return (
-    <div>How</div>
+    <>
+      <div className="">
+        <h2 className={`${styles.sectionHeadText}`}>How it works.</h2>
+        <p className={`${styles.sectionSubText}`}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam quae optio consectetur necessitatibus sed commodi veniam sequi quaerat velit, illum, exercitationem quas pariatur hic voluptate fugit itaque beatae quia earum.</p>
+      </div>
+    </>
   )
 }
 
-export default How
+export default SectionWrapper(How, 'how')

b3147062b8a1fd209251cd2400b93eeeaa2e7f60
fabrizio marras <fmarras79@gmail.com>
2023-12-01 13:29:10 +0100

    added products to list of constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -25,7 +25,7 @@ const navLinks = [
     }
 ];
 
-const benefit = [
+const benefits = [
     {
         title: "1. Benefit here",
         icon: dCloud,
@@ -48,6 +48,33 @@ const benefit = [
     },
 ];
 
+const products = [
+    {
+        title: "1. Product here",
+        icon: dCloud,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        price: "$$$"
+    },
+    {
+        title: "2. Product here",
+        icon: dCloud,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        price: "$$$"
+    },
+    {
+        title: "3. Product here",
+        icon: dCloud,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        price: "$$$"
+    },
+    {
+        title: "4. Product here",
+        icon: dCloud,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        price: "$$$"
+    },
+];
+
 const clients = [
     {
         title: "Miro",
@@ -67,4 +94,4 @@ const clients = [
     },
 ];
 
-export { navLinks, benefit, clients };
+export { navLinks, benefits, clients, products };

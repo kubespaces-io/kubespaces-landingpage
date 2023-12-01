b0d73c21c4107198216c89d53411d9237be43a2b
fabrizio marras <fmarras79@gmail.com>
2023-12-01 13:22:27 +0100

    created a higher ordered component

---
+++ src/hoc/SectionWrapper.jsx
@@ -0,0 +1,15 @@
+import { styles } from '../styles';
+
+const SectionWrapper = (Component, idName) =>
+  function HigherOrderComponent() {
+    return (
+        <section className={`${styles.padding} max-w-7xl mx-auto relative z-0`}>
+            <span className='hash-span' id={idName}>
+                &nbsp;
+            </span>
+            <Component />
+        </section>
+    )
+  }
+
+export default SectionWrapper

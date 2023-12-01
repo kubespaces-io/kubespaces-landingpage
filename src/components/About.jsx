250a277541d2673d37289c8be8a359c1ca74aab5
fabrizio marras <fmarras79@gmail.com>
2023-12-01 17:26:27 +0100

    finalised About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,18 @@
+import { styles } from '../styles';
+import Team from './Team';
+
+import { SectionWrapper } from '../hoc';
+
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <div className="">
+        <h2 className={`${styles.sectionHeadText}`}>About Us.</h2>
+        <p className={`${styles.sectionSubText}`}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam quae optio consectetur necessitatibus sed commodi veniam sequi quaerat velit, illum, exercitationem quas pariatur hic voluptate fugit itaque beatae quia earum.</p>
+      </div>
+      <Team />
+    </>
   )
 }
 
-export default About
+export default SectionWrapper(About, 'about')

9f676dfc2a06047a2d838e4b9ef0f21cac0cc633
fabrizio marras <fmarras79@gmail.com>
2023-12-01 11:41:17 +0100

    image position responsiveness on hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,9 @@ import { styles } from '../styles';
 
 const Hero = () => {
   return (
-    <section className='relative w-full h-screen mx-auto bg-hero-pattern bg-no-repeat bg-auto bg-right-bottom md:bg-contain'>
+    <section className='relative w-full h-screen mx-auto bg-hero-pattern bg-no-repeat bg-contain bg-right-bottom'>
       <div className='absolute inset-0 top-[100px] max-w-7xl mx-auto flex flex-row md:items-center items-start gap-5'>
-        <div className={`${styles.padding} max-md:bg-white/80 h-screen flex flex-col justify-start md:justify-center`}>
+        <div className={`${styles.padding} h-screen flex flex-col justify-start md:justify-center`}>
           <h1 className={`${styles.heroHeadText}`}>Production Ready <br />Cloud Environments <br />in Seconds.</h1>
           <p className={`${styles.heroSubText} max-w-2xl`}>Kubespaces is democratising access to secure, scalable, highly-available cloud compute by transforming complex cloud industry mechanisms into plug-and-play solutions.</p>
           <div className="flex flex-col xs:flex-row justify-start gap-6 mt-10">

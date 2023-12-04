3026e916d3e23c059d26579b4734ab2fd3e538e9
fabrizio marras <fmarras79@gmail.com>
2023-12-04 21:56:30 +0100

    clean up

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -10,22 +10,6 @@ const Pricing = () => {
         <h2 className={`${styles.sectionHeadText}`}>Our  Products.</h2>
         <p className={`${styles.sectionSubText}`}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam quae optio consectetur necessitatibus sed commodi veniam sequi quaerat velit, illum, exercitationem quas pariatur hic voluptate fugit itaque beatae quia earum.</p>
         <div className={`${styles.paddingY} mt-4`}>
-          {/* <ul className="grid gap-6 grid-cols-1 sm:grid-cols-2">
-            {products.map((product) => (
-              <li key={product.title} className="self-stretch">
-                <div className="p-6 md:p-10 bg-white border-primary border-2 min-h-full rounded-xl hover:shadow-xl shadow-lg">
-                  <img 
-                    src={product.icon} 
-                    alt={product.title}
-                    className={`mb-10 w-full h-full max-w-24 max-h-24 object-contain`} />
-                    <h3 className={`${styles.cardHeadText}`}>{product.title}</h3>
-                  <p className={`${styles.cardSubText}`}>{product.description}</p>
-                  <p className={`text-xl text-secondary font-bold`}>{product.price}</p>
-                </div>
-                
-              </li>
-            ))}
-          </ul> */}
           <ul className="grid gap-y-32 gap-x-6 lg:gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mt-20">
             {products.map((product) => (
               <li key={product.title} className="self-stretch">

a5d3d4397c7b13f255125733ef0ef5ff55bab72b
fabrizio marras <fmarras79@gmail.com>
2023-12-19 16:29:01 +0100

    resized the success message

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -85,9 +85,9 @@ const Contact = () => {
           <p className={styles.sectionSubText}>Get in touch.</p>
           {showSuccessMessage ? (
               <div className="mt-12 p-10 border-2 border-tertiary bg-white rounded-xl flex flex-col gap-8">
-                <img className="w-[250px]" src={success} alt="Success" />
-                <h3 className={`${styles.sectionHeadText}`}>Thank you for your message!</h3>
-                  <p className={`${styles.sectionSubText}`}>We will get back to you as soon as possible.</p>
+                <img className="w-[150px]" src={success} alt="Success" />
+                <h3 className={`${styles.cardHeadText}`}>Thank you for your message!</h3>
+                  <p className={`${styles.cardSubText}`}>We will get back to you as soon as possible.</p>
               </div>
             ) : (
               <form

913f7f3e16c716c3bb9dfc9b5078e0fa642a04cc
Alessandro Vozza <alessandro.vozza@linux.com>
2024-01-30 23:38:24 +0100

    logo

--- next.config.js
+++ next.config.js
@@ -4,3 +4,13 @@ const { withContentlayer } = require('next-contentlayer')
 const nextConfig = {}
 
 module.exports = withContentlayer(nextConfig)
+module.exports = {
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      });
+  
+      return config;
+    },
+  };

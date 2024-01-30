e5fbba449278f96cc823e7e36958a89231d61d0c
Alessandro Vozza <alessandro.vozza@linux.com>
2024-01-31 00:41:45 +0100

    module.expprts

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const { withContentlayer } = require('next-contentlayer')
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = withContentlayer(nextConfig)
 module.exports = {
+    withContentlayer(nextConfig),
     webpack(config) {
       config.module.rules.push({
         test: /\.svg$/,

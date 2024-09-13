caa4ed77eb59b9ee31675d69267c65e85fc49859
Zafar <n_zafar_007@hotmail.com>
2024-09-13 09:45:59 +0200

    header transformed

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,18 +1,32 @@
-import Link from 'next/link'
-import Dropdown from '@/components/utils/dropdown'
-import MobileMenu from './mobile-menu'
-import Logo from '@/public/images/cube.svg';
+"use client";
 
+import Link from "next/link";
+import Dropdown from "@/components/utils/dropdown";
+import MobileMenu from "./mobile-menu";
+import Logo from "@/public/images/cube.svg";
+import { useState, useEffect } from "react";
 
 export default function Header() {
+  const [top, setTop] = useState<boolean>(true);
+
+  // detect whether user has scrolled the page down by 10px
+  const scrollHandler = () => {
+    window.scrollY > 10 ? setTop(false) : setTop(true);
+  };
+
+  useEffect(() => {
+    scrollHandler();
+    window.addEventListener("scroll", scrollHandler);
+    return () => window.removeEventListener("scroll", scrollHandler);
+  }, [top]);
+
   return (
-    <header className="absolute w-full z-30">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="flex items-center justify-between h-20">
+    <header className="fixed top-2 z-30 w-full md:top-6">
+      <div className="mx-auto max-w-6xl px-4 sm:px-6">
+        <div className="relative flex h-14 items-center justify-between gap-3 rounded-2xl bg-white/90 px-3 shadow-lg shadow-black/[0.03] backdrop-blur-sm before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(theme(colors.gray.100),theme(colors.gray.200))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)]">
           {/* Site branding */}
-          <div className="shrink-0 mr-4">
-            {/* Logo */}
-            <Link href="/" className="block" aria-label="Cruip">
+          <div className="flex flex-1 items-center">
+            <Link href="/" className="inline-flex" aria-label="Cruip">
               <Logo className="w-8 h-8 fill-current text-purple-600" />
             </Link>
           </div>
@@ -20,31 +34,47 @@ export default function Header() {
           {/* Desktop navigation */}
           <nav className="hidden md:flex md:grow">
             {/* Desktop menu links */}
-            <ul className="flex grow justify-end flex-wrap items-center">
-              <li>
-                <Link href="/features" className="text-gray-300 hover:text-gray-200 px-4 py-2 flex items-center transition duration-150 ease-in-out">
+            <ul className="flex grow flex-wrap items-center justify-center gap-4 text-sm lg:gap-8">
+              <li className="px-3 py-1">
+                <Link
+                  href="/features"
+                  className="flex items-center text-gray-700 transition hover:text-gray-900"
+                >
                   Features
                 </Link>
               </li>
-              <li>
-                <Link href="/pricing" className="text-gray-300 hover:text-gray-200 px-4 py-2 flex items-center transition duration-150 ease-in-out">
+              <li className="px-3 py-1">
+                <Link
+                  href="/pricing"
+                  className="flex items-center text-gray-700 transition hover:text-gray-900"
+                >
                   Pricing
                 </Link>
               </li>
-              <li>
-                <Link href="/blog" className="text-gray-300 hover:text-gray-200 px-4 py-2 flex items-center transition duration-150 ease-in-out">
+              <li className="px-3 py-1">
+                <Link
+                  href="/blog"
+                  className="flex items-center text-gray-700 transition hover:text-gray-900"
+                >
                   Blog
                 </Link>
               </li>
+              {/* 1st level: hover */}
               <Dropdown title="Company">
                 {/* 2nd level: hover */}
                 <li>
-                <Link href="/about" className="text-gray-300 hover:text-gray-200 px-4 py-2 flex items-center transition duration-150 ease-in-out">
-                  About us
-                </Link>
-              </li>
+                  <Link
+                    href="/about"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-gray-700 hover:bg-gray-100"
+                  >
+                    About Us
+                  </Link>
+                </li>
                 <li>
-                  <Link href="/mission" className="font-medium text-sm text-gray-400 hover:text-purple-600 flex py-2 px-4 leading-tight">
+                  <Link
+                    href="/mission"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-gray-700 hover:bg-gray-100"
+                  >
                     Mission
                   </Link>
                 </li>
@@ -54,40 +84,48 @@ export default function Header() {
               <Dropdown title="Learn More">
                 {/* 2nd level: hover */}
                 <li>
-                  <Link href="/contact" className="font-medium text-sm text-gray-400 hover:text-purple-600 flex py-2 px-4 leading-tight">
-                    Contact us
+                  <Link
+                    href="/contact"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-gray-700 hover:bg-gray-100"
+                  >
+                    Contact Us
                   </Link>
                 </li>
                 <li>
-                  <Link href="/help/frequently-asked-questions" className="font-medium text-sm text-gray-400 hover:text-purple-600 flex py-2 px-4 leading-tight">
-                    Help center
+                  <Link
+                    href="//help/frequently-asked-questions"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-gray-700 hover:bg-gray-100"
+                  >
+                    Help Center
                   </Link>
                 </li>
               </Dropdown>
             </ul>
-
-            {/* Desktop sign in links */}
-            <ul className="flex grow justify-end flex-wrap items-center">
-              <li>
-                <Link
-                  href="/signin"
-                  className="font-medium text-purple-600 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out"
-                >
-                  Sign in
-                </Link>
-              </li>
-              <li>
-                <Link href="/contact" className="btn-sm text-white bg-purple-600 hover:bg-purple-700 ml-3">
-                  Sign up
-                </Link>
-              </li>
-            </ul>
           </nav>
 
-          <MobileMenu />
+          {/* Desktop sign in links */}
+          <ul className="flex flex-1 items-center justify-end gap-3">
+            <li>
+              <Link
+                href="/signin"
+                className="btn-sm bg-white text-gray-800 shadow hover:bg-gray-50"
+              >
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/signup"
+                className="btn-sm bg-gray-800 text-gray-200 shadow hover:bg-gray-900"
+              >
+                Register
+              </Link>
+            </li>
+          </ul>
 
+          <MobileMenu />
         </div>
       </div>
     </header>
-  )
+  );
 }
--- components/utils/dropdown.tsx
+++ components/utils/dropdown.tsx
@@ -1,52 +1,70 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
-import { Transition } from '@headlessui/react'
+import { useRef, useState } from "react";
+import { Transition } from "@headlessui/react";
 
 type DropdownProps = {
-  children: React.ReactNode
-  title: string
-}
+  children: React.ReactNode;
+  title: string;
+};
 
-export default function Dropdown({
-  children,
-  title
-}: DropdownProps) {
+export default function Dropdown({ children, title }: DropdownProps) {
+  const dropdownRef = useRef<HTMLUListElement | null>(null);
+  const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
 
-  const [dropdownOpen, setDropdownOpen] = useState<boolean>(false)
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  const handleFocusOut = (e: React.FocusEvent) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(e.relatedTarget as Node)
+    ) {
+      setDropdownOpen(false);
+    }
+  };
 
   return (
     <li
-      className="relative"
+      className="relative flex items-center gap-0.5 px-3 py-1"
       onMouseEnter={() => setDropdownOpen(true)}
       onMouseLeave={() => setDropdownOpen(false)}
-      onFocus={() => setDropdownOpen(true)}
-      onBlur={() => setDropdownOpen(false)}
     >
-      <a
-        className="text-gray-300 hover:text-gray-200 px-4 py-2 flex items-center transition duration-150 ease-in-out"
-        href="#0"
-        aria-expanded={dropdownOpen}
-        onClick={(e) => e.preventDefault()}
-      >
+      <span className="flex cursor-pointer items-center text-gray-700 transition hover:text-gray-900">
         {title}
-        <svg className="w-3 h-3 fill-current text-gray-500 cursor-pointer ml-1 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-          <path d="M10.28 4.305L5.989 8.598 1.695 4.305A1 1 0 00.28 5.72l5 5a1 1 0 001.414 0l5-5a1 1 0 10-1.414-1.414z" />
+      </span>
+      <button
+        className="shrink-0 p-1"
+        aria-expanded={dropdownOpen}
+        onClick={handleClick}
+      >
+        <span className="sr-only">Show submenu for "{title}"</span>
+        <svg
+          className="fill-gray-400"
+          xmlns="http://www.w3.org/2000/svg"
+          width="10"
+          height="6"
+        >
+          <path d="m1.06.19 3.5 3.5 3.5-3.5 1.061 1.06-4.56 4.56L0 1.25 1.06.19Z" />
         </svg>
-      </a>
+      </button>
       <Transition
+        ref={dropdownRef}
         show={dropdownOpen}
         as="ul"
-        className="origin-top-right absolute top-full right-0 w-40 bg-gray-800 py-2 ml-4 rounded-sm"
+        className="absolute left-0 top-full w-36 origin-top-left rounded-xl border border-gray-100 bg-white p-2 shadow-lg shadow-black/[0.03]"
         enter="transition ease-out duration-200 transform"
         enterFrom="opacity-0 -translate-y-2"
         enterTo="opacity-100 translate-y-0"
         leave="transition ease-out duration-200"
         leaveFrom="opacity-100"
         leaveTo="opacity-0"
+        onBlur={handleFocusOut}
       >
         {children}
       </Transition>
     </li>
-  )
+  );
 }

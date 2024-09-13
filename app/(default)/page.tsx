f0f5b170e11a5c47f31eeb251df6014b6ec6f53c
Zafar <n_zafar_007@hotmail.com>
2024-09-13 08:57:05 +0200

    styling init

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,26 +1,28 @@
 export const metadata = {
-  title: 'Kubespaces',
-  description: 'Kubespaces Landing page',
-}
+  title: "Kubespaces",
+  description: "Kubespaces Landing page",
+};
 
-import Hero from '@/components/hero-home'
-import Process from '@/components/process'
-import FeaturesHome from '@/components/features'
-import Tabs from '@/components/tabs'
-import Target from '@/components/target'
-import News from '@/components/news'
-import Newsletter from '@/components/newsletter'
+import Hero from "@/components/hero-home";
+import Process from "@/components/process";
+import FeaturesHome from "@/components/features";
+import Tabs from "@/components/tabs";
+import Target from "@/components/target";
+import News from "@/components/news";
+import Newsletter from "@/components/newsletter";
+import ParallaxSection from "@/components/ParallaxSection";
 
 export default function Home() {
   return (
     <>
+      {/* <ParallaxSection /> */}
       <Hero />
       <Process />
       <FeaturesHome />
       <Tabs />
       <Target />
       <News />
-      <Newsletter />      
+      <Newsletter />
     </>
-  )
+  );
 }

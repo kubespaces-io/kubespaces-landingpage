06336f73001baacb14a825e9053b7e1b13d5122b
Zafar <n_zafar_007@hotmail.com>
2024-09-14 15:59:23 +0200

    modal video left out

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -1,5 +1,5 @@
-import VideoThumb from "@/public/images/hero-image-01.jpg";
-import ModalVideo from "@/components/modal-video";
+// import VideoThumb from "@/public/images/hero-image-01.jpg";
+// import ModalVideo from "@/components/modal-video";
 
 export default function HeroHome() {
   return (

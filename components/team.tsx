dc0d7279b158f6837975bf62b4cc0dc0fb9ad6f2
Zafar <n_zafar_007@hotmail.com>
2024-09-23 14:13:15 +0200

    members info updated

--- components/team.tsx
+++ components/team.tsx
@@ -2,9 +2,10 @@ import Image from "next/image";
 
 import TeamImage01 from "@/public/images/alessandro.png";
 import TeamImage02 from "@/public/images/meg.jpeg";
-import TeamImage03 from "@/public/images/mile.jpeg";
-import TeamImage04 from "@/public/images/payam.jpeg";
-import TeamImage05 from "@/public/images/zafar.jpeg";
+import TeamImage03 from "@/public/images/thomas.jpeg";
+import TeamImage04 from "@/public/images/mile.jpeg";
+import TeamImage05 from "@/public/images/payam.jpeg";
+import TeamImage06 from "@/public/images/zafar.jpeg";
 
 export default function Team() {
   return (
@@ -49,7 +50,6 @@ export default function Team() {
                 >
                   @linkedin
                 </a>
-
               </div>
             </div>
 
@@ -83,13 +83,39 @@ export default function Team() {
             <div
               className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3"
               data-aos="fade-up"
-              data-aos-delay="200"
+              data-aos-delay="100"
               data-aos-anchor="[data-aos-id-team]"
             >
               <div className="flex flex-col items-center">
                 <Image
                   className="rounded-full mb-4"
                   src={TeamImage03}
+                  width={120}
+                  height={120}
+                  alt="Team member 02"
+                />
+                <h4 className="text-xl font-medium mb-1">Thomas Kooi</h4>
+                <div className="text-gray-500 mb-1">CTO & Co-founder</div>
+                <a
+                  className="block text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out"
+                  href="https://www.linkedin.com/in/thomasjkooi/"
+                >
+                  @linkedin
+                </a>
+              </div>
+            </div>
+
+            {/* member */}
+            <div
+              className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3"
+              data-aos="fade-up"
+              data-aos-delay="200"
+              data-aos-anchor="[data-aos-id-team]"
+            >
+              <div className="flex flex-col items-center">
+                <Image
+                  className="rounded-full mb-4"
+                  src={TeamImage04}
                   width={120}
                   height={120}
                   alt="Team member 03"
@@ -105,7 +131,7 @@ export default function Team() {
               </div>
             </div>
 
-            {/* 4th member */}
+            {/* member */}
             <div
               className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3"
               data-aos="fade-up"
@@ -115,7 +141,7 @@ export default function Team() {
               <div className="flex flex-col items-center">
                 <Image
                   className="rounded-full mb-4"
-                  src={TeamImage04}
+                  src={TeamImage05}
                   width={120}
                   height={120}
                   alt="Team member 04"
@@ -131,7 +157,7 @@ export default function Team() {
               </div>
             </div>
 
-            {/* 5th member */}
+            {/* member */}
             <div
               className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3"
               data-aos="fade-up"
@@ -141,7 +167,7 @@ export default function Team() {
               <div className="flex flex-col items-center">
                 <Image
                   className="rounded-full mb-4"
-                  src={TeamImage05}
+                  src={TeamImage06}
                   width={120}
                   height={120}
                   alt="Team member 03"
